9a9060d9a0aabede4f8cd9253d6902c34bd167d3
tonigasic <togasic@gmail.com>
2021-01-09 02:11:45 +0100

    test commit

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL: 'http://localhost:5001/toni-gasic-portfolio/us-central1/sendMail' //the local api (cloud function) url
-    baseURL: 'https://us-central1-toni-gasic-portfolio.cloudfunctions.net/sendMail' //the api (cloud function) url
+    baseURL: 'https://us-central1-toni-gasic-portfolio.cloudfunctions.net/sendMail' //the production api (cloud function) url
 })
 
 export default instance;
